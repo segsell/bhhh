458c2678b5344d7abbf1e6907854437651cf2560
MaxBlesch <maximilian.blesch@hu-berlin.de>
2024-02-03 16:51:08 -0500

    First draft.

--- tests/test_ruspy.py
+++ tests/test_ruspy.py
@@ -1,20 +1,32 @@
 import sys
-sys.path.insert(0, "ruspy/")
-import pytest
 from pathlib import Path
-import pandas as pd
+
 import numpy as np
-TEST_DIR = Path(__file__).parent
+import pandas as pd
+import pytest
 from bhhh.minimize import minimize_bhhh
+
+sys.path.insert(0, "ruspy/")
+
+TEST_DIR = Path(__file__).parent
 
 
 from ruspy.estimation.criterion_function import get_criterion_function
 
+
 @pytest.fixture()
 def input_data():
-    df_in = pd.read_pickle(TEST_DIR / "ruspy" / "docs" / "source" / "tutorials" /
-                   "replication" / "group_4.pkl")
+    df_in = pd.read_pickle(
+        TEST_DIR
+        / "ruspy"
+        / "docs"
+        / "source"
+        / "tutorials"
+        / "replication"
+        / "group_4.pkl"
+    )
     return df_in
+
 
 @pytest.fixture()
 def init_dict():
@@ -28,6 +40,7 @@ def init_dict():
         "method": "NFXP_BHHH",
     }
 
+
 def test_get_criterion_function(input_data, init_dict):
     func_dict, _ = get_criterion_function(init_dict, input_data)
     result_bhhh = minimize_bhhh(
@@ -37,7 +50,7 @@ def test_get_criterion_function(input_data, init_dict):
         convergence_absolute_gradient_tolerance=1e-12,
         stopping_max_iterations=500,
     )
-    sol = np.array([10.0749422 ,  2.29309298])
+    sol = np.array([10.0749422, 2.29309298])
     np.testing.assert_allclose(result_bhhh["solution_x"], sol, atol=1e-5)
     np.testing.assert_allclose(result_bhhh["solution_criterion"].sum(), 163.58428365)
     gradient = func_dict["criterion_derivative"](result_bhhh["solution_x"])
